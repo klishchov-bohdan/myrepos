e113965381678f443877947909b030859a5f52a6
klishchov-bohdan <bogdan.bogdan2525@gmail.com>
2021-12-10 16:25:49 +0200

    added dependencies and modified main.go file

---
+++ go.mod
@@ -0,0 +1,8 @@
+module mysite
+
+go 1.13
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/labstack/echo/v4 v4.6.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
