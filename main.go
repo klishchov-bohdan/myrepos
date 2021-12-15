8dc3f0175e9fcd8e56c27572687449e04fe909e0
klishchov-bohdan <bogdan.bogdan2525@gmail.com>
2021-12-15 15:04:12 +0200

    working with interfaces and structures

--- main.go
+++ main.go
@@ -1,27 +1,71 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"net/http"
+	"fmt"
+	"math"
 )
 
-func main() {
-	// Echo instance
-	e := echo.New()
-
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	// Routes
-	e.GET("/", hello)
-
-	// Start server
-	e.Logger.Fatal(e.Start(":8080"))
+type Rectangle struct {
+	Position
+	x2, y2 float64
 }
 
-// Handler
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+type Circle struct {
+	Position
+	r float64
+}
+
+type Position struct {
+	x, y float64
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func fullArea(s ...Shape) float64 {
+	fullArea := 0.0
+	for _, shape := range s {
+		fullArea += shape.Area()
+	}
+	return fullArea
+}
+
+func distance(x1, y1, x2, y2 float64) float64 {
+	a := x2 - x1
+	b := y2 - y1
+	return math.Sqrt(a*a + b*b)
+}
+
+func (r *Rectangle) Area() float64 {
+	l := distance(r.x, r.y, r.x, r.y2)
+	w := distance(r.x, r.y, r.x2, r.y)
+	return l * w
+}
+
+func (c *Circle) Area() float64 {
+	return math.Pi * c.r * c.r
+}
+
+func main() {
+	rect := &Rectangle{
+		Position: Position{
+			x: 0,
+			y: 0,
+		},
+		x2: 10,
+		y2: 10,
+	}
+	circ := &Circle{
+		Position: Position{
+			x: 0,
+			y: 0,
+		},
+		r: 5,
+	}
+
+	//if nil == nil {
+	//	println("here")
+	//}
+	fmt.Println(fullArea(rect, circ))
 }
